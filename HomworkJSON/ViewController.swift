87cfaaf54c69ffbb5051c014c21b0f52aaf9ae6a
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-08-08 15:28:29 +0300

    Initial Commit

---
+++ HomworkJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomworkJSON
+//
+//  Created by Daniil Lebedev on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
