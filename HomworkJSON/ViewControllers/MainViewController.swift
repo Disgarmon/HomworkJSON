6293cf3a9af3bece22a8125464fc1970af531adf
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-08-12 15:26:40 +0300

    reDid storyboard

--- HomworkJSON/ViewControllers/MainViewController.swift
+++ HomworkJSON/ViewControllers/MainViewController.swift
@@ -7,57 +7,23 @@
 
 import UIKit
 
-let predictAge = "https://api.agify.io/?name=bella"
-
 class MainViewController: UIViewController {
-}
-
-// MARK: - Private Methods
-private func successAlert() {
-    DispatchQueue.main.async {
-        let alert = UIAlertController(
-            title: "Success",
-            message: "You can see the results in the Debug aria",
-            preferredStyle: .alert
-        )
-
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alert.addAction(okAction)
+    
+    @IBOutlet var dogImageView: UIImageView!
+    @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet var randomDogButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-}
-
-private func failedAlert() {
-    DispatchQueue.main.async {
-        let alert = UIAlertController(
-            title: "Failed",
-            message: "You can see error in the Debug aria",
-            preferredStyle: .alert
-        )
-
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alert.addAction(okAction)
-    }
-}
-
-extension MainViewController{
-    private func getPredict() {
-        guard let url = URL(string: predictAge) else { return }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVS = segue.destination as? SettingsViewController else { return }
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-        
-            do {
-                let course = try JSONDecoder().decode(Predict.self, from: data)
-                print(course)
-               successAlert()
-            } catch let error {
-               failedAlert()
-                print(error)
-            }
-        }.resume()
     }
+    
+    @IBAction func getRandomDogButtonTapped(_ sender: Any) {
+    }
+    
 }
 
---
+++ HomworkJSON/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  HomworkJSON
+//
+//  Created by Daniil Lebedev on 12.08.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
